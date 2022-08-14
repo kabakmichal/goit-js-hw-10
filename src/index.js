033d51b476fc795da5e0ca77362151a309c4aab9
kabakmichal <99827389+kabakmichal@users.noreply.github.com>
2022-08-14 18:18:06 +0200

    End task
    Complete CSS

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ const countryInfo = document.querySelector('.country-info');
 const clearContent = () => {
   countryInfo.innerHTML = '';
   countryList.innerHTML = '';
-}
+};
 
-const searchCountry = (event) => {
+const searchCountry = event => {
   const findCountry = event.target.value.trim();
   if (!findCountry) {
     clearContent();
@@ -23,47 +23,51 @@ const searchCountry = (event) => {
   }
   fetchCountries(findCountry)
     .then(country => {
-    if (country.length > 10) {
-      Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-      clearContent();
-      return;
-    } else if (country.length === 1) {
-      clearContent(countryList.innerHTML);
-      renderCountryInfo(country);
-    } else if ((country.length > 1) && (country.length <= 10)) {
-      clearContent(countryInfo.innerHTML);
-      renderCountryList(country);
-    }
-  })
+      if (country.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        clearContent();
+        return;
+      } else if (country.length === 1) {
+        clearContent(countryList.innerHTML);
+        renderCountryInfo(country);
+      } else if (country.length > 1 && country.length <= 10) {
+        clearContent(countryInfo.innerHTML);
+        renderCountryList(country);
+      }
+    })
     .catch(error => {
       Notiflix.Notify.failure('Oops, there is no country with that name');
       clearContent();
       return error;
-    })
-}
+    });
+};
 
 //==========RENDERING 1 COUNTRY=========================
 const renderCountryList = country => {
-  const markup = country.map(({ name, flags }) => {
-    return `<li><img src="${flags.svg}" alt="${name.official}" width="100" height="60">${name.official}</li>`;
-  })
+  const markup = country
+    .map(({ name, flags }) => {
+      return `<li><img src="${flags.svg}" alt="${name.official}" width="100" height="60"><p>${name.official}</p></li>`;
+    })
     .join('');
   countryList.innerHTML = markup;
-}
+};
 
-//=========RENDERING 4 OR LESS COUNTRIES=================
+//=========RENDERING 10 OR LESS COUNTRIES=================
 const renderCountryInfo = country => {
   const markup = country
     .map(({ name, capital, population, flags, languages }) => {
-    return `<h1><img src="${flags.svg}" alt="${name.official
-      }" width="100" height="60">${name.official}</h1>
-      <p><span>Capital: </span>${capital}</p>
-      <p><span>Population:</span> ${population}</p>
-      <p><span>Languages:</span> ${Object.values(languages)}</p>`;
-  })
+      return `<section><h1><img src="${flags.svg}" alt="${
+        name.official
+      }" width="100" height="60">&nbsp ${name.official}</h1>
+      <p><span>Capital: </span>&nbsp ${capital}</p>
+      <p><span>Population:</span>&nbsp ${population}</p>
+      <p><span>Languages:</span>&nbsp ${Object.values(languages)}</p><section>`;
+    })
     .join('');
   countryInfo.innerHTML = markup;
-}
+};
 
 //=========EVENT LISTENER============
 inputCountry.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
